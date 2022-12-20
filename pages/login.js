d5ab81949394a3277b869d65effcc3533432b8f2
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2022-12-20 20:03:27 -0300

    feat: adding connection between login form and API

--- pages/login.js
+++ pages/login.js
@@ -1,18 +1,63 @@
 import React from 'react';
 import nookies from 'nookies';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Login({ user }) {
+  const router = useRouter();
+
   const [email, setEmail] = React.useState(user.email || '');
   const [password, setPassword] = React.useState('');
   const [viewPassword, setViewPassword] = React.useState('password');
+
+  const [error, setError] = React.useState('');
 
   return (
     <div>
       <h1>
         Login
       </h1>
-      <form>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          try {
+            const data = await fetch('/api/login', {
+              method: 'PUT',
+              body: JSON.stringify({ user: { email, password } }),
+            }).then((res) => res.json());
+
+            nookies.set(null, 'USER', JSON.stringify({ name: data.user.name, email: data.user.email }), {
+              maxAge: 1209600,
+              path: '/',
+            });
+
+            nookies.set(null, 'THEME', data.user.theme, {
+              maxAge: 1209600,
+              path: '/',
+            });
+
+            nookies.set(
+              null,
+              'REFRESH_TOKEN',
+              data.refreshToken,
+              {
+                maxAge: 1209600,
+                path: '/',
+              },
+            );
+
+            nookies.set(null, 'ACCESS_TOKEN', data.accessToken, {
+              maxAge: 60,
+              path: '/',
+            });
+
+            router.push('/gallery');
+          } catch (err) {
+            setError(err.message);
+          }
+        }}
+      >
         <input
           type="email"
           name="email"
@@ -35,6 +80,7 @@ export default function Login({ user }) {
         </button>
         <input type="submit" value="Login" />
       </form>
+      <p>{error}</p>
       <Link href="/">Home</Link>
     </div>
   );
