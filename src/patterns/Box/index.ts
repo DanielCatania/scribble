2ad467ece360dcbdc78f959f640016cbcf2dca7b
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-11-13 16:54:13 -0300

    adding box component

---
+++ src/patterns/Box/index.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Box = styled.section`
+  display: flex;
+
+  align-items: center;
+  flex-wrap: wrap;
+
+  width: 100%;
+  gap: 1em;
+  margin: 2em;
+`;
+
+export default Box;
