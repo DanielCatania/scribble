c3eac0d257d54129eaa0facf12773aff7acefec7
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-11-13 16:55:13 -0300

    adding title on home

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "@/components/Header";
 import { Main } from "./style";
+import Text from "@/patterns/Text";
+import Box from "@/patterns/Box";
+import Image from "@/patterns/Image";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Main></Main>
+      <Main>
+        <Box>
+          <Text variant="display_01">Scribble is an enabler.</Text>
+          <Image src="/img/pen.svg" width={"120px"} />
+          <Text>
+            Our mission is to facilitate your notes, help you organize your
+            projects, goals...
+          </Text>
+        </Box>
+      </Main>
     </>
   );
 }
