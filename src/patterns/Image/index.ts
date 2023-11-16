510fec49210797200e883393353bc4901df363c5
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-11-16 16:01:33 -0300

    adding inline style and max props on image

--- src/patterns/Image/index.ts
+++ src/patterns/Image/index.ts
@@ -1,15 +1,46 @@
 import styled from "styled-components";
+import { breakpoints } from "@/type";
 
 interface ImageProps {
-  width?: string;
-  height?: string;
+  width?: breakpoints<string>;
+  height?: breakpoints<string>;
+  maxWidth?: breakpoints<string>;
+  maxHeight?: breakpoints<string>;
+  inlineStyle?: breakpoints<string>;
 }
 
 const Image = styled.img.withConfig({
-  shouldForwardProp: (props) => !["width", "height"].includes(props),
+  shouldForwardProp: (props) =>
+    !["width", "height", "maxWidth", "maxHeight", "inlineStyle"].includes(
+      props
+    ),
 })<ImageProps>`
-  width: ${({ width = "" }) => width};
-  height: ${({ height = "" }) => height};
+  ${({ inlineStyle }) => inlineStyle?.xs}
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints["md"]}) {
+    ${({ inlineStyle }) => inlineStyle?.md}
+  }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints["lg"]}) {
+    ${({ inlineStyle }) => inlineStyle?.lg}
+  }
+
+  width: ${({ width }) => width?.xs};
+  height: ${({ height }) => height?.xs};
+  max-width: ${({ maxWidth }) => maxWidth?.xs};
+  max-height: ${({ maxHeight }) => maxHeight?.xs};
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints["md"]}) {
+    width: ${({ width }) => width?.md};
+    height: ${({ height }) => height?.md};
+    max-width: ${({ maxWidth }) => maxWidth?.md};
+    max-height: ${({ maxHeight }) => maxHeight?.md};
+  }
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints["lg"]}) {
+    width: ${({ width }) => width?.lg};
+    height: ${({ height }) => height?.lg};
+    max-width: ${({ maxWidth }) => maxWidth?.lg};
+    max-height: ${({ maxHeight }) => maxHeight?.lg};
+  }
 `;
 
 export default Image;
