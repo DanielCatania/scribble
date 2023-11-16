88daa6b14b75ca2f8472a568683d4488730656c4
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-11-16 15:59:15 -0300

    updating style rule in Box

--- src/patterns/Box/index.ts
+++ src/patterns/Box/index.ts
@@ -3,10 +3,10 @@ import styled from "styled-components";
 const Box = styled.section`
   display: flex;
 
+  justify-content: space-between;
   align-items: center;
   flex-wrap: wrap;
 
-  width: 100%;
   gap: 1em;
   margin: 2em;
 `;
