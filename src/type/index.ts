ba3da8b2e753464c52113c2bcd083abd8eaeb0c2
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-11-16 15:58:19 -0300

    adding breakpoints type

--- src/type/index.ts
+++ src/type/index.ts
@@ -2,3 +2,9 @@ export type color = {
   palette: "neutral" | "primary" | "secondary";
   tone: "050" | "100" | "150" | "200" | "250";
 };
+
+export type breakpoints<Type> = {
+  xs?: Type;
+  md?: Type;
+  lg?: Type;
+};
