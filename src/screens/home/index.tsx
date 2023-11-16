eed9d9abc80994548b38599c784271a1386cab60
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-11-16 16:05:21 -0300

    updating home screen title

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -11,9 +11,19 @@ export default function Home() {
       <Header />
       <Main>
         <Box>
-          <Text variant="display_01">Scribble is an enabler.</Text>
-          <Image src="/img/pen.svg" width={"120px"} />
-          <Text>
+          <Text
+            variant="display_01"
+            as="h1"
+            inlineStyle={{ md: `max-width: calc(100% - 150px);` }}
+          >
+            Scribble is an enabler.
+          </Text>
+          <Image
+            src="/img/pen.svg"
+            width={{ xs: "60px", md: "80px", lg: "120px" }}
+            inlineStyle={{ xs: `order: 2;`, md: "order: 0;" }}
+          />
+          <Text inlineStyle={{ xs: `max-width: 70%;`, md: `max-width: none;` }}>
             Our mission is to facilitate your notes, help you organize your
             projects, goals...
           </Text>
