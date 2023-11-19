ceef76091674134159fbb36b517c284c9e52509a
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-11-19 12:04:22 -0300

    adding call section on homepage

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,12 +3,19 @@ import Header from "@/components/Header";
 import Text from "@/patterns/Text";
 import Section from "@/patterns/Section";
 import Image from "@/patterns/Image";
+import Box from "@/patterns/Box";
+import Link from "@/patterns/Link";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Section as="main">
+      <Section as="main" inlineStyle={{ xs: `margin: 0;` }}>
+        <Image
+          src="/img/notes-xs.png"
+          alt="Notepad image"
+          inlineStyle={{ xs: `display: block;`, md: "display: none;" }}
+        />
         <Section>
           <Text
             variant="display_01"
@@ -26,6 +33,32 @@ export default function Home() {
             Our mission is to facilitate your notes, help you organize your
             projects, goals...
           </Text>
+        </Section>
+        <Section inlineStyle={{ xs: `width: calc(100% - 100px);` }}>
+          <Box
+            inlineStyle={{
+              xs: `
+              width: 100%;
+              flex-direction: column;
+              text-align: center;
+            `,
+              md: `max-width: 350px;`,
+              lg: `max-width: 450px;`,
+            }}
+          >
+            <Text variant="heading_02">
+              Try the most diverse functions for your organization.
+            </Text>
+            <Link href="/sign-up" style={{ size: "large" }}>
+              Use Scribble
+            </Link>
+          </Box>
+          <Image
+            src="/img/notes-md.png"
+            alt="Notepad image"
+            maxWidth={{ md: "350px" }}
+            inlineStyle={{ xs: `display: none;`, md: "display: block;" }}
+          />
         </Section>
       </Section>
     </>
