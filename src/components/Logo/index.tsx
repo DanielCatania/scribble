e0e9bba5a8ad1f2db6d0db601ccbd110abfb7e3a
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-12-01 10:51:19 -0300

    using the pattern link in the logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import NextLink from "next/link";
 import Image from "@/patterns/Image";
+import Link from "@/patterns/Link";
 
 export default function Logo() {
   return (
-    <NextLink href="/">
+    <Link href="">
       <Image
         src="/img/logo.svg"
         alt="Scribble"
         width={{ xs: "120px", md: "240px" }}
         height={{ xs: "40px", md: "80px" }}
       />
-    </NextLink>
+    </Link>
   );
 }
