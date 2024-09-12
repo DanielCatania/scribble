8ceadbe9e95ac610193ad11db3df4b6e2b5ca889
Daniel Catania Rodrigues <78505788+DanielCatania@users.noreply.github.com>
2024-09-12 19:37:30 +0000

    cleaning the project

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,9 +1,21 @@
+import React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Special+Elite&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -8,9 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        display: ["Special Elite", "Open Sans", "sans-serif"],
+        default: ["Open Sans", "sans-serif"],
       },
     },
   },
