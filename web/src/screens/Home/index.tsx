a55c850c3af75203bce805a8a6034cbb0c9fe61e
Daniel Catania Rodrigues <78505788+DanielCatania@users.noreply.github.com>
2024-09-26 19:09:03 +0000

    feat: adding colors variables in tailwind

--- web/src/screens/Home/index.tsx
+++ web/src/screens/Home/index.tsx
@@ -3,23 +3,101 @@ import React from "react";
 
 export default function HomeScreen() {
   return (
-    <div className="text-center">
-      <Text.Display as="h1" className="text-red-500">
-        Text Display
-      </Text.Display>
-      <Text.HeadingOne as="h2" className="text-blue-500">
-        Text Heading One
-      </Text.HeadingOne>
-      <Text.HeadingTwo as="h3" className="text-green-500">
-        Text Heading Two
-      </Text.HeadingTwo>
-      <Text.BodyOne as="h4" className="text-yellow-500">
-        Text Body One
-      </Text.BodyOne>
-      <Text as="h5" className="text-purple-500">
-        Text Body Two
+    <>
+      <Text className="bg-p-050 text-s-250">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
       </Text>
-      <Text>Text Default</Text>
-    </div>
+      <Text className="bg-p-100 text-s-200">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
+      </Text>
+      <Text className="bg-p-150 text-s-150">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
+      </Text>
+      <Text className="bg-p-200 text-s-100">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
+      </Text>
+      <Text className="bg-p-250 text-s-050">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
+      </Text>
+      <Text className="bg-s-050 text-p-250">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
+      </Text>
+      <Text className="bg-s-100 text-p-200">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
+      </Text>
+      <Text className="bg-s-150 text-p-150">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
+      </Text>
+      <Text className="bg-s-200 text-p-100">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
+      </Text>
+      <Text className="bg-s-250 text-p-050">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+        voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+        sint perferendis!
+      </Text>
+      <>
+        <Text className="bg-n-050 text-n-250">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+          voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+          sint perferendis!
+        </Text>
+        <Text className="bg-n-100 text-n-200">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+          voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+          sint perferendis!
+        </Text>
+        <Text className="bg-n-150 text-n-150">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+          voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+          sint perferendis!
+        </Text>
+        <Text className="bg-n-200 text-n-100">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+          voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+          sint perferendis!
+        </Text>
+        <Text className="bg-n-250 text-n-050">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          voluptate consequuntur, non ipsa repellat animi totam ex libero unde
+          voluptatem nemo enim aspernatur, deserunt mollitia odio amet tenetur
+          sint perferendis!
+        </Text>
+
+        <Text>default</Text>
+      </>
+    </>
   );
 }
