83349336c5c2a7b43c55c46e6fc13ab6ba3b9fb6
Daniel Catania Rodrigues <78505788+DanielCatania@users.noreply.github.com>
2024-09-28 00:20:48 +0000

    feat: updating home with created components

--- web/src/screens/Home/index.tsx
+++ web/src/screens/Home/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer";
 
 export default function HomeScreen() {
   return (
     <>
       <Header />
+      <main className="flex w-full flex-col items-center px-8 gap-2">
+        <HeroSection />
+        <CallToAction />
+      </main>
+      <Footer />
     </>
   );
 }
