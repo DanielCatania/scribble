992a4a6269c9ed551d266e441b537508dc6b8ecb
Daniel Catania Rodrigues <78505788+DanielCatania@users.noreply.github.com>
2024-09-28 00:44:47 +0000

    docs: documenting call to action, home component

---
+++ web/src/screens/Home/components/CallToAction/CallToAction.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import CallToAction from ".";
+
+export default {
+  title: "screens components/Home/CallToAction",
+  component: CallToAction,
+} as Meta;
+
+export const Default: StoryObj = {};
