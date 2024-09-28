dbaa0ffca38a017a55d0478c1a94d4e83047dd06
Daniel Catania Rodrigues <78505788+DanielCatania@users.noreply.github.com>
2024-09-28 00:45:00 +0000

    docs: documenting hero section, home component

---
+++ web/src/screens/Home/components/HeroSection/HeroSection.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import HeroSection from ".";
+
+export default {
+  title: "screens components/Home/HeroSection",
+  component: HeroSection,
+} as Meta;
+
+export const Default: StoryObj = {};
