f31a3aa24f7e01fd55b13a2390727ed636de6040
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2024-12-17 16:42:06 -0300

    create TokenService class

---
+++ server/src/service/Tokens/index.ts
@@ -0,0 +1,51 @@
+import { IUserTokens } from "../../type/user";
+import AppError from "../../utils/error";
+import jwt from "jsonwebtoken";
+import { z } from "zod";
+
+export default class TokenService {
+  private static getKeys() {
+    const REFRESH_TOKEN_KEY = process.env.REFRESH_TOKEN_KEY;
+    const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY;
+
+    if (!REFRESH_TOKEN_KEY || !ACCESS_TOKEN_KEY)
+      throw new AppError(500, "Absence of keys for signing tokens");
+
+    return { REFRESH_TOKEN_KEY, ACCESS_TOKEN_KEY };
+  }
+
+  static generateUserTokens(id: string) {
+    try {
+      z.string().uuid().parse(id);
+
+      const { REFRESH_TOKEN_KEY, ACCESS_TOKEN_KEY } = this.getKeys();
+
+      const refreshToken = jwt.sign({ id }, REFRESH_TOKEN_KEY, {
+        expiresIn: "7d",
+      });
+      const accessToken = jwt.sign({ id }, ACCESS_TOKEN_KEY, {
+        expiresIn: "60s",
+      });
+
+      return { accessToken, refreshToken } as IUserTokens;
+    } catch (error) {
+      return AppError.handleError(error);
+    }
+  }
+
+  static verifyRefreshToken(refreshToken: string) {
+    try {
+      const { REFRESH_TOKEN_KEY } = this.getKeys();
+
+      const payload = jwt.verify(refreshToken, REFRESH_TOKEN_KEY);
+
+      if (!payload || typeof payload === "string" || !payload.id) {
+        throw new AppError(401, "Invalid token payload");
+      }
+
+      return payload.id;
+    } catch (error) {
+      return AppError.handleError(new AppError(401, "Invalid Refresh Token"));
+    }
+  }
+}
--- server/src/service/User/index.ts
+++ server/src/service/User/index.ts
@@ -1,28 +1,17 @@
 import db from "../../repository";
-import { IUserContent, IUserCredentials, IUserTokens } from "../../type/user";
+import { IUserContent, IUserCredentials } from "../../type/user";
 import { generateSalt, createHash, generateUUID } from "../../utils/crypto";
-import jwt from "jsonwebtoken";
-import { z } from "zod";
+import TokenService from "../Tokens";
 import AppError from "../../utils/error";
 
 export default class UserService {
   static async getUserTokensByRefreshToken(refreshToken: string) {
     try {
-      const { REFRESH_TOKEN_KEY } = UserService.getKeys();
+      const id = TokenService.verifyRefreshToken(refreshToken);
 
-      try {
-        jwt.verify(refreshToken, REFRESH_TOKEN_KEY);
-      } catch (error) {
-        throw new AppError(401, "Invalid Refresh Token");
-      }
+      if (id instanceof AppError) throw id;
 
-      const payload = jwt.verify(refreshToken, REFRESH_TOKEN_KEY);
-
-      if (!payload || typeof payload === "string" || !payload.id) {
-        throw new AppError(401, "Invalid token payload");
-      }
-
-      const tokens = this.generateUserTokens(payload.id);
+      const tokens = TokenService.generateUserTokens(id);
 
       if (tokens instanceof AppError) throw tokens;
 
@@ -51,38 +40,9 @@ export default class UserService {
       if (hashedPassword !== selectedUser.password)
         throw new AppError(401, "Wrong password");
 
-      const tokens = this.generateUserTokens(selectedUser.id);
+      const tokens = TokenService.generateUserTokens(selectedUser.id);
 
       return tokens;
-    } catch (error) {
-      return AppError.handleError(error);
-    }
-  }
-
-  private static getKeys() {
-    const REFRESH_TOKEN_KEY = process.env.REFRESH_TOKEN_KEY;
-    const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY;
-
-    if (!REFRESH_TOKEN_KEY || !ACCESS_TOKEN_KEY)
-      throw new AppError(500, "Absence of keys for signing tokens");
-
-    return { REFRESH_TOKEN_KEY, ACCESS_TOKEN_KEY };
-  }
-
-  private static generateUserTokens(id: string) {
-    try {
-      z.string().uuid().parse(id);
-
-      const { REFRESH_TOKEN_KEY, ACCESS_TOKEN_KEY } = UserService.getKeys();
-
-      const refreshToken = jwt.sign({ id }, REFRESH_TOKEN_KEY, {
-        expiresIn: "7d",
-      });
-      const accessToken = jwt.sign({ id }, ACCESS_TOKEN_KEY, {
-        expiresIn: "60s",
-      });
-
-      return { accessToken, refreshToken } as IUserTokens;
     } catch (error) {
       return AppError.handleError(error);
     }
@@ -95,7 +55,7 @@ export default class UserService {
       const password = userContent.password;
       const hashedPassword = createHash(password, salt);
 
-      const tokens = UserService.generateUserTokens(id);
+      const tokens = TokenService.generateUserTokens(id);
 
       if (tokens instanceof AppError) throw tokens;
 
