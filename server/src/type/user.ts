a0421f0c35d7730abad6b1fa223815d45024ffd8
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2024-12-17 11:05:06 -0300

    implement zod validation for user content

--- server/src/type/user.ts
+++ server/src/type/user.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export interface IUserContent {
   name: string;
   email: string;
@@ -8,3 +10,18 @@ export interface IUser extends IUserContent {
   id: number;
   salt: string;
 }
+
+export const userContentSchema = z.object({
+  name: z.string().min(3, { message: "The name must have at least 3 letters" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[\p{L}\d!@#$%^&*(),.?":{}|<>]{8,}$/u,
+      {
+        message:
+          "Password must contain at least one lowercase letter, one uppercase letter, one special character, and one number.",
+      }
+    ),
+});
--- server/src/view/User/index.ts
+++ server/src/view/User/index.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import UserController from "../../controller/User";
-import { IUserContent } from "../../type/user";
+import { IUserContent, userContentSchema } from "../../type/user";
 
 export default class UserView {
   static async requestForGetUserById(
@@ -31,13 +31,24 @@ export default class UserView {
     try {
       const { content } = request.body;
 
+      try {
+        userContentSchema.parse(content);
+      } catch (error) {
+        return reply.status(400).send({ status: "400: Bad Request", error });
+      }
+
       const newUser = await UserController.createUser(content);
 
       if (typeof newUser === "string") throw new Error(newUser);
 
       reply.status(201).send({ user: newUser });
     } catch (error) {
-      reply.status(500).send(error);
+      if (error instanceof Error)
+        reply
+          .status(500)
+          .send({ status: "500: Internal Server Error", error: error.message });
+      else
+        reply.status(500).send({ status: "500: Internal Server Error", error });
     }
   }
 }
